e146a38a228b74eb9d0b0d15b388e231e3d90d2c
Haven <havencarty@gmail.com>
2020-04-21 18:46:53 +0100

    -ADDED: Error handler and form handler

--- sketch_esp8266_server.ino
+++ sketch_esp8266_server.ino
@@ -2,6 +2,8 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
+#define LED 4
+
 const char* ssid = "Mon Calamari";
 const char* password = "";
 
@@ -39,6 +41,26 @@ const char INDEX_HTML[] = // index.html string here
 "</body>"
 "</html>";
 
+const char ERROR_HTML[] = // error.html string here
+"<!DOCTYPE html>"
+"<html>"
+"<head>"
+"<title>Mon Calamari</title>"
+"<style>"
+"  body {"
+"    width: 100vw;"
+"    margin: 50px auto;"
+"    font-family: Tahoma, Verdana, Arial, sans-serif;"
+"    text-align: center;"
+"  }"
+"</style>"
+"</head>"
+
+"<body>"
+"<h1>500 - Internal Server Error</h1>"
+"</body>"
+"</html>";
+
 ESP8266WebServer server(8080); // port 80 http
 
 // This function is called whenever the root Webpage
@@ -48,8 +70,44 @@ void handleRoot()
   server.send(200, "text/html", INDEX_HTML); // 200-no error, format, html file
 }
 
+// This function is called whenever the error Webpage
+// is required. This displays the Webpage
+void handleError()
+{
+  server.send(500, "text/html", ERROR_HTML); // 500 - internal server error, format, html file
+}
+
+bool writeLED(bool LEDvalue) 
+{
+  switch(LEDvalue) {
+    case 1: digitalWrite(LED, HIGH); handleRoot();
+    case 0: digitalWrite(LED, LOW); handleRoot();
+    default: handleError();
+  }
+}
+
+//
+void handleForm()
+{
+  server.send(200, "text/html", INDEX_HTML);
+  String LEDvalue;
+
+  if (!server.hasArg("LED")) return;
+  LEDvalue = server.arg("LED");
+  if (LEDvalue == "1") {
+    writeLED(true); // illuminate LED then send HTML response
+  }
+  else if (LEDvalue == "0") {
+    writeLED(false); // turn off LED then send HTML response
+  }
+  else {
+    return; // send HTML error response
+  }
+}
+
 void setup(void) {
   Serial.begin(115200);
+  pinMode(LED, OUTPUT);
   WiFi.softAP(ssid, password); // setup access point
   Serial.println("");
 
@@ -58,6 +116,7 @@ void setup(void) {
   Serial.println(WiFi.softAPIP());
 
   server.on("/", handleRoot); // start session at root, call function handleRoot
+  server.on("/form", handleForm);
   server.begin();
 
   Serial.print("Connected to http://");
