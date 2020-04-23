b96d4ed4fb7b3c79b3af0eaa4e0be386821e9b53
Haven <havencarty@gmail.com>
2020-04-23 15:57:35 +0100

    -ADDED: Build Web Page function  -FIXED: Form for LED turn on/off

--- sketch_esp8266_server.ino
+++ sketch_esp8266_server.ino
@@ -3,7 +3,8 @@
 #include <ESP8266WebServer.h>
 #include <string.h> // string manipulation library
 
-#define LED 4
+#define LED 2 // GPIO2 = D4
+#define Port  8080
 
 const char* ssid = "Mon Calamari";
 const char* password = "";
@@ -40,7 +41,7 @@ const char FORM_HTML[] = // index.html string here
 "<b>Current=<output name=\"current\" ></output> mA</b><br><br>"
 "</form>"
 
-"<form action=\"/\" method=\"post\"><p>LED<br>"
+"<form action=\"/form\" method=\"post\"><p>LED<br>"
 "<input type=\"radio\" name=\"LED\" value=\"1\">On<br>"
 "<input type=\"radio\" name=\"LED\" value=\"0\">Off<br>"
 "<input type=\"submit\" value=\"Send\"></p>"
@@ -49,53 +50,47 @@ const char FORM_HTML[] = // index.html string here
 const char ERROR_HTML[] = // error.html string here
 "<h1>500 - Internal Server Error</h1>";
 
-ESP8266WebServer server(8080); // port 80 http
+ESP8266WebServer server(Port); // port 80 http
+
+void buildDocument(const char* HTML, int status_code)
+{
+  strcpy(html, INDEX_HTML);
+  strcat(html, HTML);
+  strcat(html, "</body></html>");
+  server.send(status_code, "text/html", html); 
+}
 
 // This function is called whenever the root Webpage
 // is required. This displays the Webpage
 void handleRoot()
 {
-  strcat(html, ROOT_HTML);
-  strcat(html, "</body></html>");
-  server.send(200, "text/html", html); // 200-no error, format, html file
+  buildDocument(ROOT_HTML, 200); // 200-no error, format, html file
 }
 
 // This function is called whenever the error Webpage
 // is required. This displays the Webpage
 void handleError()
 {
-  strcat(html, ERROR_HTML);
-  strcat(html, "</body></html>");
-  server.send(500, "text/html", html); // 500 - internal server error, format, html file
-}
-
-bool writeLED(bool LEDvalue) 
-{
-  Serial.println(LEDvalue);
-  switch(LEDvalue) {
-    case 1: digitalWrite(LED, HIGH); handleRoot();
-    case 0: digitalWrite(LED, LOW); handleRoot();
-    default: handleError();
-  }
+  buildDocument(ERROR_HTML, 500); // 500 - internal server error, format, html file
 }
 
 //
 void handleForm()
 {
-  strcat(html, FORM_HTML);
-  strcat(html, "</body></html>");
-  server.send(200, "text/html", html);
   String LEDvalue;
+  buildDocument(FORM_HTML, 200);
 
   if (!server.hasArg("LED")) return;
   LEDvalue = server.arg("LED");
-  if (LEDvalue == "1") {
-    writeLED(true); // illuminate LED then send HTML response
+  
+  if (LEDvalue == "0") {
+    digitalWrite(LED, LOW); handleForm(); // illuminate LED then send HTML response
   }
-  else if (LEDvalue == "0") {
-    writeLED(false); // turn off LED then send HTML response
+  else if (LEDvalue == "1") {
+    digitalWrite(LED, HIGH); handleForm(); // turn off LED then send HTML response
   }
   else {
+    handleError();
     return; // send HTML error response
   }
 }
@@ -104,11 +99,11 @@ void setup(void) {
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
   WiFi.softAP(ssid, password); // setup access point
-  Serial.println("");
-
-  Serial.println(""); Serial.print("Connect to ");
-  Serial.println(ssid); Serial.print("IP address: ");
-  Serial.println(WiFi.softAPIP());
+  
+  Serial.println(""); Serial.println(""); 
+  Serial.print("Connect to "); Serial.println(ssid); 
+  Serial.print("IP address: "); Serial.println(WiFi.softAPIP());
+  Serial.print("Port: ");Serial.println(Port);
 
   server.on("/", handleRoot); // start session at root, call function handleRoot
   server.on("/form", handleForm);
@@ -119,6 +114,5 @@ void setup(void) {
 }
 
 void loop(void) {
-  strcpy(html, INDEX_HTML);
   server.handleClient(); // handle any client requests
 }
